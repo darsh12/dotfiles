0a42d32af2936840d9678f654b7df752fab0c3a1
Darshan Patel <darshan7230@gmail.com>
2019-09-11 18:34:25 -0700

    Source antibody directly. Remove powerlevel9k. Remove mac specific path

--- zsh/env.sh
+++ zsh/env.sh
@@ -2,17 +2,9 @@
 
 set -o vi
 
-# Add commonly used folders to $PATH
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
-
-export PATH=$PATH:/Library/TeX/texbin:/usr/local/MacGPG2/bin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/Applications/Wireshark.app/Contents/MacOS:/Applications/Xamarin\ Workbooks.app/Contents/SharedSupport/path-bin
-
-export PATH="/usr/local/opt/ruby/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
 # Specify default editor. Possible values: vim, nano, ed etc.
 export EDITOR=vim
+
 
 # File search functions
 function f() { find . -iname "*$1*" ${@:2} }
@@ -26,7 +18,13 @@ if [ -f ~/.env_personal ]; then
 fi
 
 # Source aliases
-
-if [ -f ~/.aliases ]; then
-	source ~/.aliases
+Alias=$HOME/dotfiles/zsh/aliases
+if [ -f $Alias ]; then
+	source $Alias
 fi
+
+Antibody=$HOME/dotfiles/zsh/custom/sourcefiles.zsh
+if [ -f $Antibody ]; then
+	source $Antibody
+fi
+
