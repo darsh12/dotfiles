cb51488a4d736cefd3923f1bd6272f90465c858b
Darshan Patel <darshan7230@gmail.com>
2020-02-18 23:47:51 -0800

    Change tmux.conf to pull the location of powerline status from env, with some additional changes

--- zsh/env.sh
+++ zsh/env.sh
@@ -4,13 +4,15 @@ set -o vi
 
 # Specify default editor. Possible values: vim, nano, ed etc.
 export EDITOR=vim
+export TERM=screen-256color
+export POWERLINE_LOCATION=$(pip3 show powerline-status | grep Location | awk -F ': ' '{print $2}')
 
 
 # File search functions
 function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
 
-export TERM=screen-256color
+
 
 #Used to store personal env variables
 if [ -f ~/.env_personal ]; then
