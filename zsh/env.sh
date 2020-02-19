860e5c8b31dc5c8b4ff477fefb8911a4aec036a1
Darshan Patel <darshan7230@gmail.com>
2020-02-19 00:23:47 -0800

    Define function to update dotfiles

--- zsh/env.sh
+++ zsh/env.sh
@@ -12,6 +12,19 @@ export POWERLINE_LOCATION=$(pip3 show powerline-status | grep Location | awk -F 
 function f() { find . -iname "*$1*" ${@:2} }
 function r() { grep "$1" ${@:2} -R . }
 
+# Update function
+function dotbot() {
+    cd ~/dotfiles
+    git pull -q
+    if [[ lsb_release -eq "Darwin" ]]; then
+      ./install-profile mac
+    elif [[ lsb_release -eq "Linux" ]]; then
+        ./install-profile linux
+    else
+      echo "Invalid OS"
+      exit 1
+    fi
+}
 
 
 #Used to store personal env variables
