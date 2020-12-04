509a705a45d330e951911600e301226f69eb9c3e
darsh12 <darshan7230@gmail.com>
2020-12-04 11:04:08 -0800

    add fzf dotfile

---
+++ fzf/fzf.bash
@@ -0,0 +1,9 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/share/doc/fzf/examples/key-bindings.bash"
+
+
---
+++ fzf/fzf.zsh
@@ -0,0 +1,9 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/share/doc/fzf/examples/key-bindings.zsh"
+
+
